349658b7c23c54e49100f98f84e46926b4e50c08
Shariq Hasan <111366510+shariqhasan29@users.noreply.github.com>
2023-10-26 06:58:02 -0400

    Add files via upload

---
+++ logicfirst/Workbook/Workbook.js
@@ -0,0 +1,29 @@
+console.log('hi');
+let myName = 'Shariq'
+
+console.log(myName.charAt(Math.random()*6));
+console.log(myName.charAt(0));
+console.log(typeof (7));
+/*js intro
+typeOf
+strings methods
+ for ex
+ let myVariable = 'Shariq';
+ console.log(myVariable.length); it will show the lenght of the string 
+ .length
+ .charAt(2)
+ myVariable.indexOf('ar'); it will show the ar position like 2
+ .slice(2,4); the two value is starting index number and ending index number this method is used to slice the string according to the values given.
+ .toUppercase
+ .toLowerCase
+ myName.includes("hari"); this oncludes method will check whether the variable includes the value
+ .split(" ") it will split the spaces and stroed by array
+
+Number
+.isInteger(42); true or false
+.toFixed this is used to fix the value for the digits to be shown
+.isNaN(..)
+
+
+
+ 
